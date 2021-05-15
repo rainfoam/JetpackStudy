549af27680ec5cc282fad5ddb9497090cdab6acc
rainfoam <269011381@qq.com>
2021-05-15 08:39:24 +0800

    测试提交

---
+++ livedata/src/main/java/com/pengfei/livedata/ui/LiveDataActivity.kt
@@ -0,0 +1,12 @@
+package com.pengfei.livedata.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LiveDataActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
